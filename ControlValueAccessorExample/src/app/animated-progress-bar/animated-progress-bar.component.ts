7cb7aaa3562160056e15af8936c29a1b1a1bad5d
Joseph Beveridge <jbeverid2009@gmail.com>
2020-05-01 00:49:12 -0400

    Added Control Value Accessor

--- ControlValueAccessorExample/src/app/animated-progress-bar/animated-progress-bar.component.ts
+++ ControlValueAccessorExample/src/app/animated-progress-bar/animated-progress-bar.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, forwardRef, OnInit} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'app-animated-progress-bar',
   templateUrl: './animated-progress-bar.component.html',
-  styleUrls: ['./animated-progress-bar.component.scss']
+  styleUrls: ['./animated-progress-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => AnimatedProgressBarComponent),
+    multi: true
+  }]
 })
-export class AnimatedProgressBarComponent implements OnInit {
+export class AnimatedProgressBarComponent implements OnInit, ControlValueAccessor {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public registerOnChange(fn: any): void {
+  }
+
+  public registerOnTouched(fn: any): void {
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+  }
+
+  public writeValue(obj: any): void {
   }
 
 }
