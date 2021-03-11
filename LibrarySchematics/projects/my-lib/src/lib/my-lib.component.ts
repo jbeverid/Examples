044753424d520b36f64d8ce46f638637f26cbe50
Joe Beveridge <jbeverid2009@gmail.com>
2021-03-10 23:19:27 -0500

    Setup project to use Angular schematics.

---
+++ LibrarySchematics/projects/my-lib/src/lib/my-lib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-MyLib',
+  template: `
+    <p>
+      my-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MyLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
